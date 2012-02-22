c6acd0da08e786f3239568d5e4c39be303ac6b67
Axel Moussard <a.moussard@novactive.com>
2012-02-22 22:21:28 +0100

    Add template and FOS User

---
+++ src/AxelMoussard/BlogBundle/AxelMoussardBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AxelMoussard\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AxelMoussardBlogBundle extends Bundle
+{
+}
---
+++ src/AxelMoussard/UserBundle/AxelMoussardUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AxelMoussard\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AxelMoussardUserBundle extends Bundle
+{
+}
